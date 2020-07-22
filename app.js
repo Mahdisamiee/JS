de4a75f006ad89483b34570924da90253ebb3b3b
M23 <md.samieenia@gmail.com>
2020-07-22 19:32:16 +0430

    before part 5 challange

---
+++ app.js
@@ -0,0 +1,28 @@
+// function Person(name, lastName, age) {
+//   this.name = name;
+//   this.lastName = lastName;
+//   this.age = age;
+// }
+
+// Person.prototype.calculatedAge = function (now) {
+//   this.birthday = now - this.age;
+// };
+
+// let mahdi = new Person("mahdi", "samieenia", "22");
+// mahdi.calculatedAge(2020);
+
+// let hasan = {}
+// hasan= mahdi;
+// console.log(hasan)
+
+// console.log(Person.prototype);
+
+// let obj1 = {
+//   name: "mahdi",
+//   age: 22,
+// };
+
+// let obj2 = Object.assign({}, obj1);
+// obj2.name = "dads"
+// console.log("obj2 :>> ", obj1);
+functi
