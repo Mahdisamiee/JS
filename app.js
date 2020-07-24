1325e8d2d6e438645b0295b94d78cd474c7210f7
M23 <md.samieenia@gmail.com>
2020-07-24 19:01:31 +0430

    create new Item for html, add newItem to UI, clear fields

--- app.js
+++ app.js
@@ -25,18 +25,30 @@ let budgetController = (function () {
   };
 
   return {
-    somfunc(input) {
-      if (input.type === "inc") {
-        let incObj = new Income(
-          data.allItems.inc.length + 1,
-          input.description,
-          input.value
-        );
-        data.allItems.inc.push(incObj);
+    addItem(input) {
+      let newItem, ID;
+
+      //create new ID
+      if (data.allItems[input.type].length > 0) {
+        ID =
+          data.allItems[input.type][data.allItems[input.type].length - 1].id +
+          1;
       } else {
-        // do it for Expense Object
+        ID = 0;
       }
-      console.log(data.allItems);
+
+      //create newItem based on exp or inc
+      if (input.type === "inc") {
+        newItem = new Income(ID, input.description, input.value);
+      } else if (input.type === "exp") {
+        newItem = new Expense(ID, input.description, input.value);
+      }
+
+      // Push it into out data structure
+      data.allItems[input.type].push(newItem);
+
+      //return it to use in outher module.
+      return newItem;
     },
   };
 })();
@@ -51,6 +63,8 @@ let UIController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list",
   };
 
   return {
@@ -61,6 +75,65 @@ let UIController = (function () {
         value: document.querySelector(DOMstrings.inputValue)?.value,
       };
     },
+
+    addListItem(obj, type) {
+      let html, newHtml, element;
+      // make html code
+
+      if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+
+        html = `<div class="item clearfix" id="income-${obj.id}">
+                  <div class="item__description">${
+                    obj.description[0].toUpperCase() + obj.description.slice(1)
+                  }</div>
+                  <div class="right clearfix">
+                      <div class="item__value">+ ${obj.value}</div>
+                      <div class="item__delete">
+                          <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                      </div>
+                  </div>
+                </div>`;
+      } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+        html = `<div class="item clearfix" id="expense-${obj.id}">
+                  <div class="item__description">${
+                    obj.description[0].toUpperCase() + obj.description.slice(1)
+                  }</div>
+                  <div class="right clearfix">
+                      <div class="item__value">- ${obj.value}</div>
+                      <div class="item__percentage">10%</div>
+                      <div class="item__delete">
+                          <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                      </div>
+                  </div>
+                </div>`;
+      }
+      // replace obj vale with placeholder
+      /**
+       * here in video use ES5 way to change value of placeholder. noticethat we change it using ES6 way( object literal)
+       * newHtml = html.replace("%id%", obj.id)
+       * newHtml = html.replace("%title%", obj.description)
+       * newHtml = html.replace("%value%", obj.value)
+       */
+      // insert newHtml to our HTML file
+      newHtml = html;
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml); // this method has som security problem
+    },
+
+    clearFields() {
+      //it's return a list . list is same as array but it havnt all nice method of array.
+      let fields = document.querySelectorAll(
+        DOMstrings.inputDescription + "," + DOMstrings.inputValue
+      );
+      [...fields].forEach((current, index) => {
+        current.value = "";
+      });
+
+      // make focus on description input(first input)
+      fields[0].focus();
+    },
+
     getDom() {
       return DOMstrings;
     },
@@ -82,22 +155,28 @@ let controller = (function (budgetCtrl, UICtrl) {
   }
 
   function ctrlAddItem(params) {
+    let input, newItem;
+
     // 1- Get the Field Input data
-    let input = UICtrl.getInput();
+    input = UICtrl.getInput();
 
     // 2- Add the item to budget Controller
     Object.keys(input).map((el) => {
+      //this part is for chck to our filed be filled.
       if (input[el].trim == "") {
         // retrun error
         return;
       }
     });
-    budgetCtrl.somfunc(input);
+    newItem = budgetCtrl.addItem(input);
     // 3- Add the Item to UI
+    UICtrl.addListItem(newItem, input.type);
 
-    // 4- Calculate the Budget
+    // 4- Clear Fields
+    UICtrl.clearFields();
+    // 5- Calculate the Budget
 
-    // 5- Display the Budget on the UI
+    // 6- Display the Budget on the UI
   }
 
   return {
