2db296c5ae503c095fd5a6ac9a872bc42a0fcb11
M23 <md.samieenia@gmail.com>
2020-08-02 12:22:52 +0430

    my solution for pageination

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -7,6 +7,7 @@ export const clearInput = () => {
 };
 
 export const clearResults = () => {
+  elements.searchPage.innerHTML = "";
   while (!!elements.searchResList.lastElementChild) {
     elements.searchResList.lastChild.remove();
   }
@@ -38,6 +39,47 @@ const renderRecipe = (recipe) => {
   elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = (recipes) => {
-  recipes.forEach(renderRecipe);
+const renderButtons = (page) => {
+  const prevbtn = `
+    <button class="btn-inline results__btn--prev" data-page= "${page - 1}">
+      <svg class="search__icon">
+          <use href="img/icons.svg#icon-triangle-left"></use>
+      </svg>
+      <span>Page ${page - 1}</span>
+    </button>
+  `;
+  const nextbtn = `
+    <button class="btn-inline results__btn--next" data-page= "${page + 1}">
+      <span>Page ${page + 1}</span>
+      <svg class="search__icon">
+          <use href="img/icons.svg#icon-triangle-right"></use>
+      </svg>
+    </button>
+  `;
+  return [prevbtn, nextbtn];
+};
+const eventButton = (recipes) => {
+  elements.searchPage.addEventListener("click", (event) => {
+    let target = event.target.closest("button[data-page]");
+    if (!target) return;
+
+    clearResults();
+    renderResults(recipes, +target.dataset.page);
+  });
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  const [prev, next] = renderButtons(page);
+  if (page == 1 || page * resPerPage < recipes.length) {
+    elements.searchPage.insertAdjacentHTML("afterbegin", next);
+  }
+  if (page > 1) {
+    elements.searchPage.insertAdjacentHTML("afterbegin", prev);
+  }
+  eventButton(recipes);
 };
